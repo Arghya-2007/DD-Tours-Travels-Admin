8ee807bb2d5008b8032b2bae37a27e3202d291f8
Arghya Pal <arghya.github@gmail.com>
2026-01-31 12:33:13 +0530

    Update user

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,7 +3,7 @@ import api from "./api";
 // Accepts limit and the specific page token
 export const fetchUsers = async (limit = 10, nextPageToken = "") => {
   // Construct URL query: /users/all?limit=10&nextPageToken=abc123...
-  const query = `/users/all?limit=${limit}${nextPageToken ? `&nextPageToken=${nextPageToken}` : ""}`;
+  const query = `/users?limit=${limit}${nextPageToken ? `&nextPageToken=${nextPageToken}` : ""}`;
 
   const response = await api.get(query);
   return response.data; // Now returns { users: [...], nextPageToken: "..." }
